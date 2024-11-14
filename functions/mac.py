5a51803ec02100e2fa6a608d7f5c603a214fbc51
chiyochan <harshmaurya9278@gmail.com>
2024-11-15 03:36:42 +0530

    Add files via upload

---
+++ functions/mac.py
@@ -0,0 +1,22 @@
+import ctypes
+import sys
+import subprocess
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def run_as_admin(command):
+    if is_admin():
+        with open("mac.txt","w") as file2:
+            subprocess.run(command, check=True,text=True,stdout=file2)
+            file2.close()
+    else:
+        params = f'"{command}"'
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, f'"{sys.argv[0]}" {params}', None, 1)
+        sys.exit()
+
+command_to_run = 'getmac'
+
+run_as_admin(command_to_run)
